a4c95553d3ea072e1440640eb3b1aeb257a27d00
taminhnd <22521292@gm.uit.edu.vn>
2024-06-07 18:15:04 +0700

    add capture

---
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.ComboBox comboBoxDeviceIA;
+        private System.Windows.Forms.ComboBox comboBoxDeviceIB;
+        private System.Windows.Forms.Button buttonStartCapture;
+        private System.Windows.Forms.Button buttonStopCapture;
+        private System.Windows.Forms.ListBox listBoxPackets;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            comboBoxDeviceIA = new ComboBox();
+            comboBoxDeviceIB = new ComboBox();
+            buttonStartCapture = new Button();
+            buttonStopCapture = new Button();
+            listBoxPackets = new ListBox();
+            SuspendLayout();
+            // 
+            // comboBoxDeviceIA
+            // 
+            comboBoxDeviceIA.FormattingEnabled = true;
+            comboBoxDeviceIA.Location = new Point(12, 12);
+            comboBoxDeviceIA.Name = "comboBoxDeviceIA";
+            comboBoxDeviceIA.Size = new Size(889, 28);
+            comboBoxDeviceIA.TabIndex = 0;
+            // 
+            // comboBoxDeviceIB
+            // 
+            comboBoxDeviceIB.FormattingEnabled = true;
+            comboBoxDeviceIB.Location = new Point(12, 46);
+            comboBoxDeviceIB.Name = "comboBoxDeviceIB";
+            comboBoxDeviceIB.Size = new Size(889, 28);
+            comboBoxDeviceIB.TabIndex = 1;
+            // 
+            // buttonStartCapture
+            // 
+            buttonStartCapture.Location = new Point(12, 80);
+            buttonStartCapture.Name = "buttonStartCapture";
+            buttonStartCapture.Size = new Size(430, 42);
+            buttonStartCapture.TabIndex = 2;
+            buttonStartCapture.Text = "Start Capture";
+            buttonStartCapture.UseVisualStyleBackColor = true;
+            buttonStartCapture.Click += buttonStartCapture_Click;
+            // 
+            // buttonStopCapture
+            // 
+            buttonStopCapture.Location = new Point(448, 80);
+            buttonStopCapture.Name = "buttonStopCapture";
+            buttonStopCapture.Size = new Size(453, 42);
+            buttonStopCapture.TabIndex = 3;
+            buttonStopCapture.Text = "Stop Capture";
+            buttonStopCapture.UseVisualStyleBackColor = true;
+            buttonStopCapture.Click += buttonStopCapture_Click;
+            // 
+            // listBoxPackets
+            // 
+            listBoxPackets.AllowDrop = true;
+            listBoxPackets.FormattingEnabled = true;
+            listBoxPackets.Location = new Point(12, 128);
+            listBoxPackets.Name = "listBoxPackets";
+            listBoxPackets.Size = new Size(889, 244);
+            listBoxPackets.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(913, 387);
+            Controls.Add(comboBoxDeviceIA);
+            Controls.Add(comboBoxDeviceIB);
+            Controls.Add(buttonStartCapture);
+            Controls.Add(listBoxPackets);
+            Controls.Add(buttonStopCapture);
+            Name = "Form1";
+            Text = "Network Tamper";
+            Load += Form1_Load;
+            ResumeLayout(false);
+        }
+    }
+}
---
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -0,0 +1,98 @@
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PacketDotNet;
+using SharpPcap;
+using SharpPcap.LibPcap;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        private ICaptureDevice deviceIA;
+        private ICaptureDevice deviceB;
+        private CaptureDeviceList devices = CaptureDeviceList.Instance;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            foreach (var dev in devices)
+            {
+                comboBoxDeviceIA.Items.Add(dev.Description);
+                comboBoxDeviceIB.Items.Add(dev.Description);
+            }
+        }
+
+        private void buttonStartCapture_Click(object sender, EventArgs e)
+        {
+            if (comboBoxDeviceIA.SelectedIndex == -1 || comboBoxDeviceIB.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please select both network interfaces.");
+                return;
+            }
+
+            var devices = CaptureDeviceList.Instance;
+
+            deviceIA = devices[comboBoxDeviceIA.SelectedIndex];
+            //deviceIB = devices[comboBoxDeviceIB.SelectedIndex];
+
+
+
+            Task.Run(() => StartPacketCapture());
+        }
+
+        private void buttonStopCapture_Click(object sender, EventArgs e)
+        {
+            deviceIA.Close();
+            //deviceIB?.Close();
+        }
+
+        private void StartPacketCapture()
+        {
+            try
+            {
+                deviceIA.Open();
+                deviceIA.OnPacketArrival += (sender, e) =>
+                {
+                    try
+                    {
+                        var packet = Packet.ParsePacket(e.GetPacket().LinkLayerType, e.GetPacket().Data);
+
+                        if (packet is EthernetPacket ethernetPacket)
+                        {
+                            if (ethernetPacket.PayloadPacket is IPPacket ipPacket)
+                            {
+                                if (ipPacket.PayloadPacket is TcpPacket tcpPacket)
+                                {
+                                    var sourceIp = ipPacket.SourceAddress;
+                                    var destinationIp = ipPacket.DestinationAddress;
+                                    var sourcePort = tcpPacket.SourcePort;
+                                    var destinationPort = tcpPacket.DestinationPort;
+
+                                    listBoxPackets.Items.Add($"Packet: Source IP: {sourceIp}, Source Port: {sourcePort}, Destination IP: {destinationIp}, Destination Port: {destinationPort}");
+                                    listBoxPackets.TopIndex = listBoxPackets.Items.Count - 1;
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error processing packet: {ex.Message}");
+                    }
+                };
+
+                deviceIA.StartCapture();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error capturing traffic: {ex.Message}");
+            }
+        }
+    }
+}
